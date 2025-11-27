6e91196b639cc6ce620ec55e448127984a5c6a54
Jeremypoh05 <Jeremypoh0205@gmail.com>
2025-11-27 21:26:40 +0800

    Azure Deploying Testing

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,23 @@ function App() {
 
  return (
    <div className="p-10 text-3xl font-bold text-blue-600">
-     Hello Tailwind v4
-     <h2>I am Jeremy!</h2>
+     <main className="max-w-6xl mx-auto px-6">
+       <section id="home" className="min-h-screen flex items-center">
+        <h1>H1 Section</h1>
+       </section>
+
+       <section id="about" className="py-28">
+       </section>
+
+       <section id="experience" className="py-28">
+       </section>
+
+       <section id="projects" className="py-28">
+       </section>
+
+       <section id="contact" className="py-28">
+       </section>
+     </main>
    </div>
  );
 }
